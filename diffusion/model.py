8a533dfc2b9cb63113e1d0578679d29720247817
Patrick Lindemann <patrick.felix.lindemann@campus.tu-berlin.de>
2023-11-14 01:03:11 +0100

    Added program skeleton

---
+++ diffusion/model.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+import torch
+
+
+class DiffusionModel(torch.nn.Model):
+    """_summary_
+
+    Parameters
+    ----------
+    torch : _type_
+        _description_
+    """
+
+    def __init__(self) -> None:
+        """_summary_"""
+        pass
+
+    def forward(self, X: torch.Tensor) -> Any:
+        """_summary_
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            _description_
+
+        Returns
+        -------
+        Any
+            _description_
+        """
+        pass
