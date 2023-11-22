6ffeab0e4d3191bec49aba7c13a36d77e280be07
Patrick Lindemann <patrick.felix.lindemann@campus.tu-berlin.de>
2023-11-22 01:09:53 +0100

    Added quadratic scheduler

---
+++ diffusion/schedule/quadratic_scheduler.py
@@ -0,0 +1,47 @@
+import torch
+
+from .scheduler import Scheduler
+
+
+class QuadraticScheduler(Scheduler):
+    """__summary__"""
+
+    start: float
+    end: float
+
+    def __init__(
+        self,
+        start: float = 0.0,
+        end: float = 1.0,
+    ) -> None:
+        """_summary_
+
+        Parameters
+        ----------
+        start : float, optional
+            _description_, by default 0.0
+        end : float, optional
+            _description_, by default 1.0
+        """
+        self.start = start
+        self.end = end
+
+    def __call__(self, t: torch.Tensor) -> torch.Tensor:
+        """_summary_
+
+        Parameters
+        ----------
+        t : torch.Tensor
+            _description_. Between 0 and 1
+
+        Returns
+        -------
+        torch.Tensor
+            _description_
+        """
+        assert torch.all(t >= 0.0) and torch.all(t <= 1.0)
+        f = lambda x: x**2
+        v_start = f(self.start)
+        v_end = f(self.end)
+        v_t = f(t * (self.end - self.start) + self.start)
+        return (v_t - v_start) / (v_end - v_start)
