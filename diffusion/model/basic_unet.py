b3c93c7f1662af71601e9f4764721aebed2fb8f3
pdcnguyen <phamdangcaonguyen@gmail.com>
2023-11-22 17:36:43 +0100

    .

--- diffusion/model/basic_unet.py
+++ diffusion/model/basic_unet.py
@@ -1,71 +1,80 @@
-from typing import List
-
 import torch
+import torchvision.transforms.functional as TF
+from torch import nn
+from torch.nn import functional as F
 
 
-class BasicUNet(torch.nn.Module):
-    """__summary__"""
-
-    down_layers: torch.nn.ModuleList
-    up_layers: torch.nn.ModuleList
-    activation_function: torch.nn.SiLU
-    downscale: torch.nn.MaxPool2d
-    upscale: torch.nn.Upsample
-
-    def __init__(self, channels_in: int = 1, channels_out: int = 1) -> None:
-        """_summary_
-
-        Parameters
-        ----------
-        in_channels : int, optional
-            _description_, by default 1
-        out_channels : int, optional
-            _description_, by default 1
-        """
-        super().__init__()
-        self.down_layers = torch.nn.ModuleList(
-            [
-                torch.nn.Conv2d(channels_in, 32, kernel_size=5, padding=2),
-                torch.nn.Conv2d(32, 64, kernel_size=5, padding=2),
-                torch.nn.Conv2d(64, 64, kernel_size=5, padding=2),
-            ]
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
         )
-        self.up_layers = torch.nn.ModuleList(
-            [
-                torch.nn.Conv2d(64, 64, kernel_size=5, padding=2),
-                torch.nn.Conv2d(64, 32, kernel_size=5, padding=2),
-                torch.nn.Conv2d(32, channels_out, kernel_size=5, padding=2),
-            ]
-        )
-        self.activation_func = torch.nn.SiLU()
-        self.downscale = torch.nn.MaxPool2d(2)
-        self.upscale = torch.nn.Upsample(scale_factor=2)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """_summary_
+    def forward(self, x):
+        return self.conv(x)
 
-        Parameters
-        ----------
-        x : torch.Tensor
-            _description_
 
-        Returns
-        -------
-        torch.Tensor
-            _description_
-        """
-        # Residual connections
-        h: List[torch.Tensor] = []
-        # Downward pass
-        for i, down_layer in enumerate(self.down_layers):
-            x = self.activation_func(down_layer(x))
-            if i < 2:
-                h.append(x)
-                x = self.downscale(x)
-        # Upward pass
-        for i, up_layer in enumerate(self.up_layers):
-            if i > 0:
-                x = self.upscale(x[None, :, :, :])[0]
-                x += h.pop()
-            x = self.activation_func(up_layer(x))
+# Implementation inspired by Aladdin Persson - PyTorch Image Segmentation Tutorial with U-NET: everything from scratch baby
+class BasicUnet(nn.Module):
+    def __init__(
+        self,
+        in_channels=3,
+        out_channels=3,
+        features=[64, 128, 256, 512],
+    ):
+        # UNET Convolution
+        super(BasicUnet, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+            # Up part of UNET
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    feature * 2,
+                    feature,
+                    kernel_size=2,
+                    stride=2,
+                )
+            )
+            self.ups.append(DoubleConv(feature * 2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1] * 2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx // 2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:], antialias=True)
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx + 1](concat_skip)
+
+        x = self.final_conv(x)
+
         return x
