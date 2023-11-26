24eed5be7dfb53124476861d69e4db821ef89618
Patrick Lindemann <patrick.felix.lindemann@campus.tu-berlin.de>
2023-11-26 16:44:54 +0100

    Renamed reader.py to loader.py

--- diffusion/data/__init__.py
+++ diffusion/data/__init__.py
@@ -1,4 +1,4 @@
-from .reader import load_data, load_image
+from .loader import load_data, load_image
 from .transform import reverse_transform_image, transform_image
 from .visualizer import show_image
 from .writer import save_image
