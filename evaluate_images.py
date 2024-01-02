75fb897a22a452b3d483619f4fe722404fce8088
Patrick Lindemann <patrick.felix.lindemann@campus.tu-berlin.de>
2024-01-03 01:59:16 +0200

    Added routine to evaluate generated images using the inception score

---
+++ evaluate_images.py
@@ -0,0 +1,48 @@
+import argparse
+import logging
+import os
+import pathlib
+
+import torch
+
+from diffusion.data import load_images
+from diffusion.eval import calculate_inception_score
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "image_dir",
+        type=pathlib.Path,
+        help="The path to folder containing the images to evaluate.",
+    )
+    parser.add_argument(
+        "--device",
+        type=str.lower,
+        help='The device to use.\nAllowed values: "cpu", "cuda".',
+        default="cuda" if torch.cuda.is_available() else "cpu",
+    )
+    parser.add_argument(
+        "--verbose", action="store_true", help="Enable verbose logging."
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # Parse the arguments
+    args = get_args()
+    device = torch.device(args.device)
+
+    # Prepare the logger
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
+    # Load the images
+    logging.info(f"Loading images from {args.image_dir}.")
+    images = load_images(args.image_dir, device=device)
+    logging.info(f"Loaded {len(images)} images.")
+
+    # Calculate the inception scores
+    logging.info("Calculating inception score.")
+    score, _ = calculate_inception_score(images, batch_size=10, device=device)
+    # Round to 4 decimal places
+    logging.info(f"Inception score: {score:.4f}")
